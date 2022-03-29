50100264f4677aa7a996a1ba7e69da59195c0505
katiehickman <katie.hickman2704@gmail.com>
2022-03-29 13:51:49 -0700

    cat animation added to menu

---
+++ Replit_Code/week_2/animation3.py
@@ -0,0 +1,70 @@
+# imports
+import time
+import os
+
+Color34 = "\u001b[34m"
+Color37 = "\u001b[37m"
+
+def cat1(): # cat number 1 in the animation
+    print("|\---/|")
+    print("| ,_, |")
+    print(" \_`_/-..----.")
+    print("___/ `   ' ,""+ \  ")
+    print("(__...'   __\    |`.___.';")
+    print("(_,...'(_,.`__)/'.....+")
+    print("\u001b[34m -------------------------------------------- \u001b[37m")
+
+def cat2():  # cat number 2 in the animation
+    print("   |\---/|")
+    print("   | ,_, |")
+    print("    \_`_/-..----.")
+    print("   ___/ `   ' ,""+ \  ")
+    print("   (__...'   __\    |`.___.';")
+    print("   (_,...'(_,.`__)/'.....+")
+    print("\u001b[34m -------------------------------------------- \u001b[37m")
+
+def cat3():  # cat number 3 in the animation
+    print("      |\---/|")
+    print("      | ,_, |")
+    print("       \_`_/-..----.")
+    print("      ___/ `   ' ,""+ \  ")
+    print("      (__...'   __\    |`.___.';")
+    print("      (_,...'(_,.`__)/'.....+")
+    print("\u001b[34m -------------------------------------------- \u001b[37m")
+
+def cat4():  # cat number 4 in the animation
+    print("         |\---/|")
+    print("         | ,_, |")
+    print("          \_`_/-..----.")
+    print("         ___/ `   ' ,""+ \  ")
+    print("         (__...'   __\    |`.___.';")
+    print("         (_,...'(_,.`__)/'.....+")
+    print("\u001b[34m -------------------------------------------- \u001b[37m")
+
+def cat5():  # cat number 5 in the animation
+    print("            |\---/|")
+    print("            | ,_, |")
+    print("             \_`_/-..----.")
+    print("            ___/ `   ' ,""+ \  ")
+    print("            (__...'   __\    |`.___.';")
+    print("            (_,...'(_,.`__)/'.....+")
+    print("\u001b[34m -------------------------------------------- \u001b[37m")
+
+# moves the cat
+os.system("clear")
+time.sleep(.1)
+cat1()
+time.sleep(.5)
+os.system("clear")
+cat2()
+time.sleep(.5)
+os.system("clear")
+cat3()
+time.sleep(.5)
+os.system("clear")
+cat4()
+time.sleep(.5)
+os.system("clear")
+cat5()
+time.sleep(.5)
+os.system("clear")
