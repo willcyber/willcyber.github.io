b2568d85947337df4210c74ba2c931731e4f3594
William <wutwilliam@gmail.com>
2024-09-28 12:32:02 -0700

    testing another template

--- src/App.js
+++ src/App.js
@@ -1,45 +1,16 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import Main from "./containers/Main";
-import { ThemeProvider } from "styled-components";
-import { themes } from "./theme";
-import { GlobalStyles } from "./global";
-import { CursorProvider } from "react-cursor-custom";
-import { settings } from "./portfolio";
-import ReactGA from "react-ga";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
-  useEffect(() => {
-    if (settings.googleTrackingID) {
-      ReactGA.initialize(settings.googleTrackingID, {
-        testMode: process.env.NODE_ENV === "test",
-      });
-      ReactGA.pageview(window.location.pathname + window.location.search);
-    }
-  }, []);
-
-  const [theme, setTheme] = useState(localStorage.getItem("theme") || "dark");
-  const useCursor = settings.useCustomCursor;
-
   return (
-    <ThemeProvider theme={themes[theme]}>
-      <>
-        <GlobalStyles />
-        <div>
-          {useCursor ? (
-            <CursorProvider
-              color={themes[theme].secondaryText}
-              ringSize={25}
-              transitionTime={75}
-            >
-              <Main theme={themes[theme]} setTheme={setTheme} />
-            </CursorProvider>
-          ) : (
-            <Main theme={themes[theme]} setTheme={setTheme} />
-          )}
-        </div>
-      </>
-    </ThemeProvider>
+    <>
+      <Navbar />
+      <Header />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
