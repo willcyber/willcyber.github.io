3c34465bcf5f49876c2a1382bcbf9319d9df7588
William <wutwilliam@gmail.com>
2024-09-28 10:04:52 -0700

    testing out new website template

--- Replit_Code/week_2/sqrt.py
+++
@@ -1,8 +0,0 @@
-import math
-def run():
-
-  num = float(input(" Enter a number: "))
-  sqRoot = math.pow(num, 0.5)
-  # determines the square root of given value
-  print("The square root of {0} is {1}".format(num, sqRoot)) 
-  # prints the square root
